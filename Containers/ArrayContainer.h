933704c547f011db26f0adc6417c761ea7903add
CaglayanDokme <caglayandokme@gmail.com>
2021-03-19 19:26:39 +0300

    Fill constructor added.
    No discard attribute added to related functions.
    Subscript operators added.

--- Containers/ArrayContainer.h
+++ Containers/ArrayContainer.h
@@ -17,28 +17,53 @@
 /** Libraries **/
 #include <cstddef>
 
+/** Special definitions **/
+#if __cplusplus >= 201703l	// If the C++ version is greater or equal to 2017xx
+#define NODISCARD [[nodiscard]]
+#else
+#define NODISCARD
+#endif
+
+/*** Container Class ***/
 template<class T, size_t SIZE>
 class Array{
 public:
 	/*** Constructors and Destructors ***/
-	Array() = default;
+	Array() noexcept = default;			// Default constructor
+	Array(const T& fillValue) noexcept;	// Fill constructor
+
 	~Array() = default;
 
 	/*** Iterators ***/
 	using iterator 			= T*;
 	using const_iterator 	= const T*;
 
-	iterator begin() 				{ return data; 			}
-	iterator end() 					{ return data + SIZE; 	}
-	const_iterator cbegin() const 	{ return data; 			}
-	const_iterator cend() const		{ return data + SIZE; 	}
+	NODISCARD iterator begin() 					{ return data; 			}
+	NODISCARD iterator end() 					{ return data + SIZE; 	}
+	NODISCARD const_iterator cbegin() const 	{ return data; 			}
+	NODISCARD const_iterator cend() const		{ return data + SIZE; 	}
+
+	/*** Operators ***/
+	NODISCARD const T& operator[](const size_t index) const { return data[index]; }
+	NODISCARD T& operator[](const size_t index) 			{ return data[index]; }
 
 	/*** Status Checkers ***/
-	constexpr size_t getSize() { return size; }
+	NODISCARD constexpr size_t getSize() { return size; }
 
 private:
 	T data[SIZE];
 	const size_t size = SIZE;
 };
+
+/**
+ * @brief	Fill constructor fills every element with a copy of the given one.
+ * @param 	fillValue	Reference value for filling.
+ */
+template<class T, size_t SIZE>
+Array<T, SIZE>::Array(const T& fillValue) noexcept
+{
+	for(T& element : data)
+		element = fillValue;
+}
 
 #endif // Recursive inclusion preventer
