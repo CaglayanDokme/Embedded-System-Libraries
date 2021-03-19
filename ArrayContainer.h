44e648a2b5bb9b6602e5ad8a377cc603a0452f34
CaglayanDokme <caglayandokme@gmail.com>
2021-03-19 11:26:20 +0300

    Initial commit.

---
+++ ArrayContainer.h
@@ -0,0 +1,17 @@
+/**
+ *  @file       ArrayContainer.h
+ *  @details    A template container class with an underlying array data structure.
+ *              This container is designed for embedded devices with limited resources.
+ *              Dynamic allocation is not preferred to prevent fragmentation and some other performance related problems.
+ *  @author     Caglayan DOKME, caglayandokme@gmail.com
+ *  @date       February 23, 2021 -> First release
+ *
+ *  @note       Feel free to contact for questions, bugs, improvements or any other thing.
+ *  @copyright  No copyright.
+ */
+
+/** Recursive inclusion preventer **/
+#ifndef ARRAY_CONTAINER_H
+#define ARRAY_CONTAINER_H
+
+#endif // Recursive inclusion preventer endif
