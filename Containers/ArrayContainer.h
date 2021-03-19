8c5e2ba4b4cc04da9d19a3476b0d99c58369311d
CaglayanDokme <caglayandokme@gmail.com>
2021-03-19 18:35:26 +0300

    First working release.
    Tried on Arty Z7 board with ZYNQ.
    Development environment is Linux with Vitis 2020.1.
    Compiler is ARM G++

--- Containers/ArrayContainer.h
+++ Containers/ArrayContainer.h
@@ -4,7 +4,7 @@
  *              This container is designed for embedded devices with limited resources.
  *              Dynamic allocation is not preferred to prevent fragmentation and some other performance related problems.
  *  @author     Caglayan DOKME, caglayandokme@gmail.com
- *  @date       February 23, 2021 -> First release
+ *  @date       March 19, 2021 -> First release
  *
  *  @note       Feel free to contact for questions, bugs, improvements or any other thing.
  *  @copyright  No copyright.
@@ -14,4 +14,31 @@
 #ifndef ARRAY_CONTAINER_H
 #define ARRAY_CONTAINER_H
 
-#endif // Recursive inclusion preventer endif
+/** Libraries **/
+#include <cstddef>
+
+template<class T, size_t SIZE>
+class Array{
+public:
+	/*** Constructors and Destructors ***/
+	Array() = default;
+	~Array() = default;
+
+	/*** Iterators ***/
+	using iterator 			= T*;
+	using const_iterator 	= const T*;
+
+	iterator begin() 				{ return data; 			}
+	iterator end() 					{ return data + SIZE; 	}
+	const_iterator cbegin() const 	{ return data; 			}
+	const_iterator cend() const		{ return data + SIZE; 	}
+
+	/*** Status Checkers ***/
+	constexpr size_t getSize() { return size; }
+
+private:
+	T data[SIZE];
+	const size_t size = SIZE;
+};
+
+#endif // Recursive inclusion preventer
