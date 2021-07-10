d8ab65e7af57fbe2b99015a0fc090e5ca75ef665
CaglayanDokme <caglayandokme@gmail.com>
2021-07-10 12:04:23 +0300

    Doxygen added. Copy constructor added.

--- Containers/QueueContainer.h
+++ Containers/QueueContainer.h
@@ -3,7 +3,8 @@
  * @details     A template queue container class for embedded systems.
  *              The contatiner is implemented without any dynamic allocation feature.
  * @author      Caglayan DOKME, caglayandokme@gmail.com
- * @date        July 9, 2021 -> First release
+ * @date        July 9, 2021    -> First release
+ *              July 10, 2021   -> Doxygen added. Copy constructor added.
  * @note        Feel free to contact for questions, bugs or any other thing.
  * @copyright   No copyright.
  */
@@ -40,6 +41,9 @@ public:
     // Default constructor
     Queue();
 
+    // Copy Constructor
+    Queue(const Queue& copyQ);
+
     /*** Element Access ***/
     NODISCARD const_reference front() const;
     NODISCARD reference       front();
@@ -70,10 +74,31 @@ private:
     void IncrementIndex(size_type& index) { index = (index + 1) % SIZE; }
 };
 
+/**
+ * @brief Default constructor
+ */
 template<class T, std::size_t SIZE>
 Queue<T,SIZE>::Queue()
     : sz(0), idxFront(0), idxBack(SIZE-1)
 { /* No operation */ }
+
+/**
+ * @brief Copy constructor
+ */
+template<class T, std::size_t SIZE>
+Queue<T,SIZE>::Queue(const Queue& copyQ)
+    : sz(0), idxFront(0), idxBack(SIZE-1)
+{
+    if(copyQ.empty() == false)
+    {
+        for(size_type elemIdx = 0; elemIdx < copyQ.sz; ++elemIdx)
+            data[elemIdx] = copyQ.data[(copyQ.idxFront + elemIdx) % SIZE];
+
+        idxFront    = 0;
+        idxBack     = copyQ.sz - 1;
+        sz          = copyQ.sz;
+    }
+}
 
 /**
  * @brief   Returns a constant reference to the front element of Queue
@@ -115,6 +140,11 @@ T& Queue<T, SIZE>::back()
     return data[idxBack];
 }
 
+/**
+ * @brief   Pushes the element to the Queue
+ * @param   value   Constant lValue reference to the object to be pushed
+ * @note    Overwrites the oldest element in the Queue.
+ */
 template<class T, std::size_t SIZE>
 void Queue<T, SIZE>::push(const value_type& value)
 {
@@ -132,6 +162,11 @@ void Queue<T, SIZE>::push(const value_type& value)
     sz += full() ? 0 : 1;
 }
 
+/**
+ * @brief   Pushes the element to the Queue
+ * @param   value   rValue reference to the object to be pushed
+ * @note    Overwrites the oldest element in the Queue.
+ */
 template<class T, std::size_t SIZE>
 void Queue<T, SIZE>::push(value_type&& value)
 {
@@ -149,6 +184,11 @@ void Queue<T, SIZE>::push(value_type&& value)
     sz += full() ? 0 : 1;
 }
 
+/**
+ * @brief   Pushes the element by constructing it in-place with the given arguments
+ * @param   args  Arguments to be forwarded to the constructor of the new element
+ * @note    Overwrites the oldest element in the Queue.
+ */
 template<class T, std::size_t SIZE>
 template <class... Args>
 void Queue<T, SIZE>::emplace(Args&&... args)
@@ -167,6 +207,9 @@ void Queue<T, SIZE>::emplace(Args&&... args)
     sz += full() ? 0 : 1;
 }
 
+/**
+ * @brief Pops the front element of the Queue
+ */
 template<class T, std::size_t SIZE>
 void Queue<T, SIZE>::pop()
 {
@@ -179,6 +222,10 @@ void Queue<T, SIZE>::pop()
     }
 }
 
+/**
+ * @brief Swaps the content of two Queues
+ * @param swapQ     Queue to be swapped with
+ */
 template<class T, std::size_t SIZE>
 void Queue<T, SIZE>::swap(Queue& swapQ)
 {
